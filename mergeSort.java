ea1e2ed3f2b14db527866ce82057c8917d4e311b
Dawijak04 <23384549@studentmail.ul.ie>
2023-11-01 11:52:18 +0000

    smellie ellie

---
+++ mergeSort.java
@@ -0,0 +1,17 @@
+package Day15;
+
+public class mergeSort {
+
+	public static void main(String[] args) {
+		int[] randArray = {1,2,3,4,5,6,7};
+		int low = 1;
+		int high = ((randArray.length) - 1);
+		int mid;
+		
+		
+	if (low < high) {
+		mid = ((low+high)/2);
+		
+	}
+	}
+}
